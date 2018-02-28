79215e8c05da56c700483c87450f2387d29d18a2
EvgeniyTT <tatarin.e.m@gmail.com>
2018-03-01 00:05:08 +0200

    add several components from material-ui

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
+import ProductCard from './components/ProductCard'
+import Navbar from './components/Navbar'
+import FilterList from './components/FilterList'
 import logo from './logo.svg';
 import './App.css';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar />
+        <FilterList />
+        <ProductCard />
       </div>
     );
   }
---
+++ src/utils/db.js
@@ -0,0 +1,55 @@
+export const categories = [
+  {
+    id: 0,
+    name: 'bags',
+    label: 'Bags'
+  },
+  {
+    id: 1,
+    name: 'backpacks',
+    label: 'Backpacks'
+  },
+  {
+    id: 2,
+    name: 'accessories',
+    label: 'Accessories'
+  },
+  {
+    id: 3,
+    name: 'luggage',
+    label: 'Luggage'
+  }
+]
+
+export const colors = [
+  {
+    id: 0,
+    name: 'domino',
+    label: 'Domino'
+  },
+  {
+    id: 1,
+    name: 'scuba',
+    label: 'Scuba'
+  },
+  {
+    id: 2,
+    name: 'derby',
+    label: 'Derby'
+  },
+  {
+    id: 3,
+    name: 'flamingo',
+    label: 'Flamingo'
+  },
+  {
+    id: 4,
+    name: 'scout',
+    label: 'Scout'
+  },
+  {
+    id: 5,
+    name: 'roulette',
+    label: 'Roulette'
+  }
+]
